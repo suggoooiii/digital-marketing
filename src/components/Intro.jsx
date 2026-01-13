dc716df73a4865e1a19cc1c8d335b7e05a214343
ramighammami@gmail.com <ramighammami@gmail.com>
2026-01-13 15:56:54 +0200

    got rid of bg color form theme, added heroSectionimg

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,7 @@
 import { useScroll, useTransform, motion } from "framer-motion";
 import { useRef } from "react";
-import laptop0 from "/src/assets/img/laptop0.jpg";
+// import laptop0 from "/src/assets/img/laptop0.jpg";
+import heroimg from "/src/assets/img/heroSectionimg.jpeg";
 import { TextHoverEffect } from "./ui/text-hover-effect";
 export default function Intro() {
   const container = useRef();
@@ -15,20 +16,11 @@ export default function Intro() {
   return (
     <div className="h-screen overflow-hidden" ref={container}>
       <div className="absolute z-10 w-full h-full flex items-center justify-center">
-        <motion.h1
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          style={{ opacity }}
-          transition={{ duration: 0.8, delay: 0.3 }}
-          className="text-[7vw] uppercase font-mono text-center z-20 mix-blend-difference m-0"
-        >
-          <TextHoverEffect text="Kay" />
-          Marketing Agency
-        </motion.h1>
+        <TextHoverEffect text="Kay" />
       </div>
       <motion.div style={{ y }} className="relative h-full">
         <img
-          src={laptop0}
+          src={heroimg}
           fill
           alt="image"
           style={{ objectFit: "cover" }}
